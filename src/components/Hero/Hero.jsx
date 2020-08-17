584a350538613123d102ca0027d933b93caf9fba
nofamex <56834455+nofamex@users.noreply.github.com>
2020-08-17 23:49:55 +0700

    contexting component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,11 +3,15 @@ import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
+import TextTransition, { presets } from 'react-text-transition';
+
+const Texts = ['Software Engineer', 'Learning Enthusiast', 'Seasonal Gamer', 'Full Time Weebs'];
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
-  const { title, name, subtitle, cta } = hero;
+  const { title, name, cta } = hero;
 
+  const [index, setIndex] = useState(0);
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -21,6 +25,10 @@ const Header = () => {
     }
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => setIndex((index) => index + 1), 3000);
+  }, []);
+
   return (
     <section id="hero" className="jumbotron">
       <Container>
@@ -29,14 +37,17 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Your Name'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            <span>
+              I Am The{' '}
+              <TextTransition text={Texts[index % Texts.length]} springConfig={presets.slow} />
+            </span>
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta || 'Know me more'}
               </Link>
             </span>
           </p>
--- src/mock/data.js
+++ src/mock/data.js
@@ -17,49 +17,64 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
+  img: 'image.png',
+  paragraphOne:
+    'Hello my name is nofaldi, but you can call me opal, i am currently pursuing a Computer Science degree at University of Indonesia.',
+  paragraphTwo:
+    'I usually spend my free time reading books or manga, (im a weeeb :3). I love to code and i also like to teach to other people.',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1ZVCAwSXMrvw6w7sfFVQUcXISGaDkQGKh/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project.png',
+    title: 'Online Chat App',
+    info:
+      'Online chat application made with React, Redux, Express and Socket.io, this project mainly made to implement the use of socket.io, each user can create a room to chat or join another people room by simply inputting the room name.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://5f32c096df41472f47cd8676--hardcore-mahavira-d3f484.netlify.app/',
+    repo: 'https://github.com/nofamex/Chat-App-FrontEnd', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project2.png',
+    title: 'Netflix Clone',
+    info:
+      'Netflix clone made with React and Redux, this application is using the TMDB API to fetch the movie data and showing it on the application, this app doesnt play any movie but show the trailer instead when one of the film is clicked.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://netflix-clone-3e31b.web.app/',
+    repo: 'https://github.com/nofamex/Netflix-Clone', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project3.png',
+    title: 'I Am Okay',
+    info:
+      'This website made with Django and python for my organization where i was working, my organization mainly focus on educating about mental health, and i with my team was in charge of website development.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'http://www.iamokay.id/',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project4.png',
+    title: 'Online Shop',
+    info:
+      'Build with Django and JavaScript, this website is my final project for my web development class in college, this app is build by implementing red-green-refractor and fully tested in unit test and functional test using selenium.',
+    info2: '',
+    url: 'https://tugaskelompok1.herokuapp.com/',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Would you like to hire me? Cool bro !',
+  btn: 'Contact Me',
+  email: 'nofaldi.fikrul@ui.ac.id',
 };
 
 // FOOTER DATA
@@ -68,27 +83,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/nofelldown',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/nofaldi-atmam-b34851196/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/nofamex',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
